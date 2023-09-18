b5def66487129281c8e9ad09ce78c783a63cbfda
csf123321 <2652212759@qq.com>
2023-09-18 23:12:54 +0800

    登录的云函数

---
+++ cloudfunctions/matchusersdata/index.js
@@ -0,0 +1,49 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+}) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const db = cloud.database()
+  const userCollection = db.collection('userInfo')
+  const phonenumber = event.phonenumber
+  const StudentID = event.StudentID
+  const OpenID = wxContext.OPENID
+  const RegisterFlag = event.flag
+  const result = await userCollection.where({StudentID}).get()
+  const phone = String(result.data[0].PhoneNum)
+  const oid2 = result.data[0].OpenID
+    if (phone == phonenumber && RegisterFlag == "1" && oid2 == "") {
+      console.log(userCollection.where({StudentID:StudentID}).update({data:{OpenID:OpenID}}))
+      await userCollection.where({StudentID:StudentID}).update({data:{OpenID:OpenID}}).then(res=>{
+        console.log(res)
+      })
+      return {
+        code: "2",
+        message: '用户信息注册成功'
+      }
+    } else if(phone == phonenumber && RegisterFlag == "0" && result.data[0].OpenID == OpenID){
+      return {
+        code: "0",
+        message: '用户信息匹配成功'
+      }
+    }else{
+      return {
+        code:"-1",
+        message:"用户信息匹配失败"
+      }
+    }
+
+  return {
+    event:event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+    code: -1,
+    message: '出现错误，请稍后重试'
+  }
+}
