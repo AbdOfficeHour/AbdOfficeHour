78815cbd7f168a1099ac117dfda9e80a85e42bd1
MZ YANG <123012548+HOWARD-mzYANG@users.noreply.github.com>
2023-09-19 19:25:47 +0800

    第一阶段完毕
    做好了无预约时的页面

--- miniprogram/pages/appointmentList/appointmentList.js
+++ miniprogram/pages/appointmentList/appointmentList.js
@@ -5,9 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    todos: [], // 用户的所有待办事项
+    pending: [], // 未完成待办事项
+    finished: [], // 已完成待办事项
+    list: [],
+    items: false, //当下是否有预约
+    showModal:false,
+    color: 'red',
+    zh_cn: 1,
+    en: 0,
+    credit: 1
   },
-
+  
+  goTo:function(){
+    wx.navigateTo({
+      url: '../appointment/appointment',
+    })
+  },
+  
+  
   /**
    * 生命周期函数--监听页面加载
    */
