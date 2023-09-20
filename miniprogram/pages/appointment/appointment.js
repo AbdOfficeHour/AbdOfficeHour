a28b85b39ea722b263622abff3ba0321e38186f8
MZ YANG <123012548+HOWARD-mzYANG@users.noreply.github.com>
2023-09-21 01:24:55 +0800

    学生端大体做好了
    细节需要改进，如自动刷新，提示是否提交成功

--- miniprogram/pages/appointment/appointment.js
+++ miniprogram/pages/appointment/appointment.js
@@ -1,3 +1,4 @@
+let toast;
 Page({
 
   /**
@@ -6,12 +7,12 @@ Page({
   data: {
     zh_cn: 1,
   //picker 测试
-    array: ['美国', '中国', '巴西', '日本'],
-    date: ['9.21','9.28'],
-    time: ['14:00-14:30','14:30-15:00','15:00-15:30'],
+    array: [],
+    date: [],
+    time: [],
   //下面是数据类型
-    name: "Li Yuanfang",
-    phone: "18600000000",
+    name: "",
+    phone: "",
     state1: false,//现在老师信息没填
     state2: false,//现在日期没填
     state3: false,//现在时间没填
@@ -21,19 +22,161 @@ Page({
     day: "",
     hour: "",
     tips: "",
+    success:"",
+    message:"",
+  },
+  
+
+
+  get_lang(){
+    wx.cloud.callFunction({
+      name: 'getLanguage',    //这里写云函数名称
+      data: "",
+      
+      success:res=>{
+          this.setData({
+            zh_cn:res.result.language
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
+  },
+
+  add_appoint(){
+    wx.cloud.callFunction({
+      name: 'addEvent',    //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          //这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
   },
 
 
-//这是按下按钮之后的函数，按理来说上传数据也应该从这走
+  //拿取name和tele的函数
+  get_user(){
+    wx.cloud.callFunction({
+      name: 'getUserInfo',   //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            name:res.result.name,
+            phone:res.result.phoneNum
+            
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          console.log("读取失败")//这里是失败的回调函数
+      }
+    })
+  },
+
+
+  //第一步，选择老师
+  sl_tea(){
+    wx.cloud.callFunction({
+      name: 'getSelection',    //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            array:res.result
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
+  },
+  //第二步，选择date
+  sl_dat(){
+    wx.cloud.callFunction({
+      name: 'getSelection',    //这里写云函数名称
+      data: {
+          teacher:this.data.teacher,//这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            date:res.result
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
+  },
+  //第三步，选择time
+  sl_tim(){
+    wx.cloud.callFunction({
+      name: 'getSelection',    //这里写云函数名称
+      data: {
+          teacher:this.data.teacher,
+          date:this.data.day//这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            time:res.result
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
+  },
+  //第四步，加上tips，上传云端
+  add_app(){
+    wx.cloud.callFunction({
+      name: 'addEvent',   //这里写云函数名称
+      data: {
+          teacher:this.data.teacher,
+          date:this.data.day,
+          time:this.data.hour,
+          tips:this.data.tips
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            success:res.result.success,
+            message:res.result.message,
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          console.log('上传失败')//这里是失败的回调函数
+      }
+    })
+  },
+
+  //这是按下按钮之后的函数，按理来说上传数据也应该从这走
   goTo:function(){
-    var state = this.data.state1&&this.data.state2&&this.data.state3;
-    if (state)
+    this.add_app()
+    //传送走是最后一步！！！
+    setTimeout(()=>
+   {
+     if (this.data.success)
     { 
       wx.switchTab({
         url: '../home/home'
       })
       console.log(this.data.tips)
     }
+   }, 2000)
+    
   },
 
   bindPickerChange1: function (e) {
@@ -46,6 +189,7 @@ Page({
       teacher: this.data.array[e.detail.value],
       state1: true,
     })
+    this.sl_dat()
   },
 
 
@@ -60,6 +204,7 @@ Page({
       day: this.data.date[e.detail.value],
       state2: true,
     })
+    this.sl_tim()
   },
 
   bindPickerChange3: function (e) {
@@ -85,8 +230,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.get_lang()
     //先调用云函数把老师都存进一个数组
-    var array1 = new Array();
+    this.sl_tea()
+    this.get_user()
     
   },
 
--- miniprogram/pages/appointmentList/appointmentList.js
+++ miniprogram/pages/appointmentList/appointmentList.js
@@ -6,18 +6,118 @@ Page({
    */
   data: {
     items: true, //无预约时为TRUE，默认true
-    zh_cn: 1,
+    zh_cn: 0,
     credit: 1,
     list:[],//存放预约记录
     test: true,
+    //弹窗的数据
+    std_name:"",
+    std_tele:"",
+    state: 0,
+    visible: false,
+	  buttons: [{
+	  	text: 'Confirm',
+		  color: '#FF2B2B'
+    }],
   },
   
+  get_credit()
+  {
+    wx.cloud.callFunction({
+      name: 'getCredit',    //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            credit:res.result.Credit
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+  })
+  },
+
+  get_lang(){
+    wx.cloud.callFunction({
+      name: 'getLanguage',    //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            zh_cn:res.result.language
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          //这里是失败的回调函数
+      }
+    })
+  },
+
+  //拿取预约的函数
+  get_info(){
+    wx.cloud.callFunction({
+      name: 'getEventData',   //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            list:res.result,
+            
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          console.log("读取失败")//这里是失败的回调函数
+      }
+    })
+  },
+  //拿取name和tele的函数
+  get_user(){
+    wx.cloud.callFunction({
+      name: 'getUserInfo',   //这里写云函数名称
+      data: {
+          //这里填写发送的数据
+      },
+      
+      success:res=>{
+          this.setData({
+            std_name:res.result.name,
+            std_tele:res.result.phoneNum
+            
+          })//这里是成功的回调函数
+      },
+      fail:err=>{
+          console.log("读取失败")//这里是失败的回调函数
+      }
+    })
+  },
+
+
   goTo:function(){
     wx.navigateTo({
       url: '../appointment/appointment',
     })
   },
   
+  onclick(e){
+    console.log(e)
+    console.log(e.currentTarget.dataset.value)
+    this.setData({
+      visible: true,
+      state: e.currentTarget.dataset.value,
+    })
+  },
+  onTap(e){
+    this.setData({
+      visible: false,
+    })
+  },
   
   /**
    * 生命周期函数--监听页面加载
@@ -25,30 +125,20 @@ Page({
   onLoad(options) {
     /**发送本地openid，云函数上传*/
     /**学生和老师都需要读取相同的信息 */
-    /**step1：看看有多少次*/
-    var count = "10";
-    var appoint = new Array();
-    /**step2：for循环,调用云函数*/
-    for (var i = 0; i < count; i++)
+    this.get_credit()
+    this.get_lang()
+    this.get_user()
+    this.get_info()
+    setTimeout(()=>
     {
-      appoint[i]={
-        std_name : "Mingzhe Yang",
-        std_tele  : "1860000000",
-        teacher: "Ruizhe Li",
-        date : "9.21",
-        time: "14:00-14:30",
-        tips:"这是一段注释",
-      };
-      
-    }
-    this.setData({
-      list:appoint,
-    })
-    if(count > 0){
-      this.setData({
-        items: false,
-      })
-    }
+      if (this.data.list.length > 0)
+      { 
+        this.setData({
+          items:false
+        })
+        console.log(this.data.items)
+      }
+    }, 2000)
   },
 
   /**
