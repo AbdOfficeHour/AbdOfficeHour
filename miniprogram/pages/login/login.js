dffc209f1606f2d9f735f05b9ed07923c4c7835f
u15sl22 <1354828493@qq.com>
2023-09-21 20:18:24 +0800

    学生端登陆界面最终版
    hihi

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -11,11 +11,50 @@ Page({
       show:false,
       article:'',
       flag2:"-1",
+      credit:1,
       buttons: [{
-        text: '确定',
+        text: 'yes',
         color: '#FF2B2B'
-      }]
-  },
+      }],
+      language: 0
+    },
+
+    chinese: function (){
+        this.setData({
+            language: 1
+        })
+        // wx.cloud.callFunction({
+        //     name: "",
+        //     data:{
+        //         language: 1
+        //     },
+        //     success:res=>{
+        //         console.log("成功提交")
+        //     },
+        //     fail:err=>{
+        //         console.log("未提交，提交报错")
+        //     }
+        // })
+    },
+
+    english: function (){
+        this.setData({
+            language: 0
+        })
+        // wx.cloud.callFunction({
+        //     name: "",
+        //     data:{
+        //         language: 0
+        //     },
+        //     success:res=>{
+        //         console.log("成功提交")
+        //     },
+        //     fail:err=>{
+        //         console.log("未提交，提交报错")
+        //     }
+        // })
+    },
+
     changeText: function (e) {
       // console.log(e)
       this.setData({
@@ -66,7 +105,7 @@ Page({
             wx.setStorageSync('Name', res.result.Name)
             // console.log(res)
             wx.switchTab({
-              url: '/pages/home/index',
+              url: '/pages/home/home',
             })
           }
           this.setData({
@@ -78,29 +117,29 @@ Page({
   
   
     matchusersdata: async function () {
-      this.setData({
-        buttdisable:true
-      })
-      if (this.data.phonenumber == "" || this.data.StudentID == ""){
-        wx.showModal({
-          title: '警告',
-          content: '请填写学号或手机号',
-          complete: (res) => {
-            if (res.cancel) {
-              this.setData({
-                buttdisable:false
-              })
-            }
-        
-            if (res.confirm) {
-              this.setData({
-                buttdisable:false
-              })
-            }
-          }
+        this.setData({
+          buttdisable:true
         })
-        return false
-      }
+        if (this.data.phonenumber == "" || this.data.StudentID == ""){
+          wx.showModal({
+            title: '警告',
+            content: '请输入正确的数据',
+            complete: (res) => {
+              if (res.cancel) {
+                this.setData({
+                  buttdisable:false
+                })
+              }
+          
+              if (res.confirm) {
+                this.setData({
+                  buttdisable:false
+                })
+              }
+            }
+          })
+          return false
+        }
       // console.log('先执行数据匹配')
       // 调用云函数 matchusersdata
       // console.log('openId=', this.data.openID),
@@ -127,7 +166,7 @@ Page({
             flag2:res.result.code
           })
           if (true) {
-            if(res.result.code == "2"){
+            if(res.result.code == "2"||res.result.code =="0"){
             wx.cloud.callFunction({
               name: 'checkuserexist',
               data: {
@@ -150,9 +189,117 @@ Page({
               }
             })
           }else{
+              console.log(res.result.code)
             wx.showModal({
               title: '验证失败',
               content: '请输入正确的手机号和学号！',
+              }
+            )
+            this.setData({
+              buttdisable:false
+            })
+          }}
+          else{
+          }
+        },
+        fail: err => {
+          console.log(err)
+          wx.showModal({
+            title: '警告',
+            content: '请正确填写你的手机号和学号',
+            complete: (res) => {
+              if (res.cancel) {
+                
+              }
+          
+              if (res.confirm) {
+                
+              }
+              this.setData({
+                buttdisable:false
+              })
+            }
+          })
+        }
+      })
+    },
+    warning: async function () {
+        this.setData({
+          buttdisable:true
+        })
+        if (this.data.phonenumber == "" || this.data.StudentID == ""){
+          wx.showModal({
+            title: 'warning',
+            content: 'please enter your data first',
+            complete: (res) => {
+              if (res.cancel) {
+                this.setData({
+                  buttdisable:false
+                })
+              }
+          
+              if (res.confirm) {
+                this.setData({
+                  buttdisable:false
+                })
+              }
+            }
+          })
+          return false
+        }
+      // console.log('先执行数据匹配')
+      // 调用云函数 matchusersdata
+      // console.log('openId=', this.data.openID),
+      //   console.log(
+      //     this.data.flag,
+      //     this.data.studentID,
+      //     this.data.phonenumber,
+      //     this.data.openID
+      //   ),
+        // console.log('即将调用云函数matchusersdata.....')
+        
+      wx.cloud.callFunction({
+        name: 'matchusersdata',
+        data: {
+          flag: this.data.flag,
+          StudentID: this.data.studentID,
+          phonenumber: this.data.phonenumber,
+          openID: this.data.openID
+        },
+        success: res => {
+          //console.log('res=', res,'flag=',this.data.flag)
+          // 匹配成功后跳转到下一个页面
+          this.setData({
+            flag2:res.result.code
+          })
+          if (true) {
+            if(res.result.code == "2"||res.result.code =="0"){
+            wx.cloud.callFunction({
+              name: 'checkuserexist',
+              data: {
+                openID: this.data.openID
+              }
+  
+              ,
+              success: res => {
+                // console.log('checkuserexist', res);
+                if(res.result.result == "0"){
+        
+                  wx.setStorageSync('StudentID', String(res.result.StudentID))
+                  wx.setStorageSync('phoneNum', String(res.result.PhoneNum))
+                  wx.setStorageSync('Name', res.result.Name)
+                  this.showPopup()
+                }
+                this.setData({
+                  flag: res.result.result
+                })
+              }
+            })
+          }else{
+              console.log(res.result.code)
+            wx.showModal({
+              title: 'failed',
+              content: 'wrong information！',
               }
             )
             this.setData({
@@ -204,7 +351,7 @@ Page({
       })
       if(this.data.flag2 != "-1"){
         wx.switchTab({
-          url: '/pages/home/index',
+          url: '/pages/home/home',
         })
       }
     },
