bbf4ce71f4276f20c440d2efc84c901cb72b4022
csf123321 <2652212759@qq.com>
2023-09-25 21:09:23 +0800

    做了一半

--- cloudfunctions/loadExcel/index.js
+++ cloudfunctions/loadExcel/index.js
@@ -3,6 +3,12 @@ const cloud = require('wx-server-sdk')
 const xlsx = require('node-xlsx')
 const moment = require('moment')
 
+const mapName = {
+  "教师姓名":"Name",
+  "教师SCNU账号":"SCNUID",
+  "教师手机号":"phoneNum"
+}
+
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 
 // 云函数入口函数
@@ -10,47 +16,28 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
   const db = cloud.database()
-  const fileID = "cloud://cloud1-7gq5naln5bf65474.636c-cloud1-7gq5naln5bf65474-1321030591/测试用.xlsx"
+  const fileID = "cloud://cloud1-7gq5naln5bf65474.636c-cloud1-7gq5naln5bf65474-1321030591/Example.xlsx"
 
   const res = await cloud.downloadFile({
     fileID:fileID
   })
 
   console.log("下载的文件->",res)
-  var files = (xlsx.parse(res.fileContent,{cellDates:true}))[0].data
-
-  var T,times
-  T = files[0][0]
-  times = 1
-  for(var i=0;i<T;i++){
-    var name = files[times][0]
-    var N = files[times][1]
-    times++
-    var tmp = {}
-    for(var j=0;j<N;j++){
-      var dateContext = moment(files[times][0]).add(1,'days').format("MM/DD")
-      tmp[dateContext] = {}
-      files[times].forEach(item=>{
-        if(typeof item=="string")
-        {
-          tmp[dateContext][item] = 1
-        }
-      })
-      times++
+  var files = (xlsx.parse(res.fileContent,{cellDates:true}))
+  console.log(files)
+  
+  //读取sheet1
+  var sheet1 = files[0].data
+  var teacherObjList = []
+  for(var i=1;i<sheet1.length&&sheet1[i].length;i++){
+    if(!sheet1[i])break
+    var teacherObj = {}
+    for(var j=0;j<sheet1[i].length&&sheet1[i][j];j++){
+      teacherObj[mapName[sheet1[0][j]]] = sheet1[i][j]
     }
-
-    await db.collection("teachers").where({Name:name}).update({
-      data:{
-        TimeTable:tmp
-      }
-    })
+    teacherObjList.push(teacherObj)
   }
 
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  console.log(teacherObjList)
+  
 }
