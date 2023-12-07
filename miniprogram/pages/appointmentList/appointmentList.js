2c3edbb05c49c9ff832625155da533b83c49a188
初乎草庭 <1783541507@qq.com>
2023-12-07 17:39:18 +0800

    修复撤回bug

--- miniprogram/pages/appointmentList/appointmentList.js
+++ miniprogram/pages/appointmentList/appointmentList.js
@@ -211,7 +211,7 @@ Page({
     x.setFullYear(a.substring(0,4),a.substring(5,7)-1,a.substring(8,10))
     var today = new Date();
     today.setDate(today.getDate()+1);
-    if ((this.data.list[num].state == 6)||(this.data.list[num].state == 4)||(this.data.list[num].state == 5)||(today >= x && this.data.list[num].state == 2)||(today > x && this.data.list[num].state == 3))
+    if ((this.data.list[num].state == 6)||(this.data.list[num].state == 4)||(this.data.list[num].state == 5)||(today > x && this.data.list[num].state == 2)||(today > x && this.data.list[num].state == 3))
     {
       this.setData({
         buttons_zh_cn:[{
