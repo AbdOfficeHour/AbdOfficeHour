acbf094358b0b1245b1e63a3a61d705e7ff2f32c
初乎草庭 <1783541507@qq.com>
2023-12-08 15:47:40 +0800

    修改开发环境V1.1.8版本完成
    但是忘记该app,json了......

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -30,8 +30,8 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        //env: 'formalcloud-1g5snmwa3dc92179',
-        env:"developcloud-3gz9urho4a43b008",
+        env: 'formalcloud-1g5snmwa3dc92179',
+        //env:"developcloud-3gz9urho4a43b008",
         traceUser: true,
       });
     }
