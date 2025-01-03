682f71ba722859627310168885604c48d54c8626
csf123321 <2652212759@qq.com>
2025-01-03 20:17:58 +0800

    修改为通过分页逐步查询预约事件

--- cloudfunctions/DownLoadExcel/index.js
+++ cloudfunctions/DownLoadExcel/index.js
@@ -19,26 +19,33 @@ exports.main = async (event, context) => {
   const db = cloud.database()
   const _ = db.command
 
-  var events = (await db.collection("events").get()).data
-
-  var date = new Date()
-  var dataCVS = `events/${date.getTime()}.xlsx`
-
+  var events = []
+  var offsetI = 0
   var allData = []
-  var row = ["教师","学生","日期","时间","备注","状态","拒绝原因","工作总结"]
-  allData.push(row)
-  for(var i = 0;i<events.length;i++){
-    var arr = []
-    arr.push(events[i].teacher)
-    arr.push(events[i].Student)
-    arr.push(`${events[i].dateTime.getMonth()+1}/${events[i].dateTime.getDate()}`)
-    arr.push(events[i].time)
-    arr.push(events[i].Note?events[i].Note:events[i].note)
-    arr.push(states[events[i].state])
-    arr.push(events[i].reasons_for_refusal)
-    arr.push(events[i].workSummary)
-    allData.push(arr)
-  }
+
+  do{
+    var events = (await db.collection("events").limit(100).skip(100*offsetI).get()).data
+
+    var date = new Date()
+    var dataCVS = `events/${date.getTime()}.xlsx`
+
+    var row = ["教师","学生","日期","时间","备注","状态","拒绝原因","工作总结"]
+    allData.push(row)
+    for(var i = 0;i<events.length;i++){
+      var arr = []
+      arr.push(events[i].teacher)
+      arr.push(events[i].Student)
+      arr.push(`${events[i].dateTime.getMonth()+1}/${events[i].dateTime.getDate()}`)
+      arr.push(events[i].time)
+      arr.push(events[i].Note?events[i].Note:events[i].note)
+      arr.push(states[events[i].state])
+      arr.push(events[i].reasons_for_refusal)
+      arr.push(events[i].workSummary)
+      allData.push(arr)
+    }
+
+    offsetI++;
+  }while(events.length > 0);
 
   var buffer = await xlsx.build([{
     name:"sheet1",
